f029043d4d2da946ebf84b940a5b3774fd49e88e
fanselmo <agenciapenseglobal@gmail.com>
2023-07-10 15:31:58 -0300

    Imagens incluidas e conclusão do codigo

--- main.js
+++ main.js
@@ -7,10 +7,22 @@ $(document).ready(function() {
         $('form').slideUp()
     })
     
-
     $('form').on('submit', function(e){
-        alert('submit')
         e.preventDefault()
-    })
+        const enderecoNovaImagem = $('#endereco-imagem-nova').val()
+        const novoItem = $('<li style="display: none"><li>')
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem)
+        $(`
+        <div class="overlay-img-link">
+            <a href="${enderecoNovaImagem}" target="_blank" title="Ver imagem em tamanho Real">
+            Ver imagem em tamanho real
+            </a>
+        </div>
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(1000)
+        $('#endereco-imagem-nova').val('')
+    }) 
         
 })
+
